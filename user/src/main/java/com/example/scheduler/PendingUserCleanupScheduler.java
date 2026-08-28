cd049991f297d3dfdedea397a2a092d441a8d268
Life-with0and1 <ayuuayuu0se1@gmail.com>
2026-08-28 17:22:03 +0530

    Add Scheduler for PendingRegister Cleanup

---
+++ user/src/main/java/com/example/scheduler/PendingUserCleanupScheduler.java
@@ -0,0 +1,22 @@
+package com.example.scheduler;
+
+import com.example.user.repository.PendingRegisterRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class PendingUserCleanupScheduler {
+
+    private final PendingRegisterRepository pendingRegisterRepository;
+
+    public PendingUserCleanupScheduler(PendingRegisterRepository pendingRegisterRepository) {
+        this.pendingRegisterRepository = pendingRegisterRepository;
+    }
+
+    @Scheduled(fixedRate = 600000) 
+    public void cleanupExpiredUsers() {
+        pendingRegisterRepository.deleteByExpirationTimeBefore(LocalDateTime.now());
+    }
+}
--- user/src/main/java/com/example/user/repository/PendingRegisterRepository.java
+++ user/src/main/java/com/example/user/repository/PendingRegisterRepository.java
@@ -1,9 +1,21 @@
 package com.example.user.repository;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.user.model.PendingRegisterUser;
 
+import io.lettuce.core.dynamic.annotation.Param;
+import jakarta.transaction.Transactional;
+
 public interface PendingRegisterRepository extends JpaRepository<PendingRegisterUser, Long> {
     public void deleteAllByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM PendingRegisterUser p WHERE p.expirationTime < :now")
+    void deleteByExpirationTimeBefore(@Param("now") LocalDateTime now);
 }
